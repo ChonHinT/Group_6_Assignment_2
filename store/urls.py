4db01ac88721b67825eee80a7f7c2e2ee5aa0bc0
ChonHinT <139086407+ChonHinT@users.noreply.github.com>
2023-07-10 20:40:04 +1000

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views 
+from .views import search_product
 
 
 app_name = 'store'
@@ -8,4 +9,5 @@ urlpatterns = [
     path('', views.products_all, name='product_all'),
     path('<slug:slug>', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
+    path('search/', views.search_product, name = 'search_product'),
 ]
