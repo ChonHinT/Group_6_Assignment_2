73892aa27ee64ffb70c3c7f23b25eaec30482654
JL3s <127005979+JL3s@users.noreply.github.com>
2023-08-10 17:27:08 +0930

    Update forms.py
    search input validation

--- store/forms.py
+++ store/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.validators import RegexValidator
 
 class ProductSearchForm(forms.Form):
-    search_query = forms.CharField(max_length=100)
+    search_query = forms.CharField(max_length=100,
+                                  validators=[RegexValidator('[;&|$<>...]', inverse_match=True)])
